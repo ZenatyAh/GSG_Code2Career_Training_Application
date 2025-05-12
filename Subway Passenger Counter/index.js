89f07f02b93d861962153f266e241a473e4bd0cb
ZenatyAh <zenatyah@gmail.com>
2025-05-12 13:45:50 +0300

    Finish Subway Passenger Counter Project

---
+++ Subway Passenger Counter/index.js
@@ -0,0 +1,15 @@
+let count = 0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count_el");
+
+const increment = () => {
+    count += 1;
+    countEl.textContent = count;
+}
+
+const save = () => {
+    let countSrt = count + ' - ';
+    saveEl.textContent += countSrt;
+    countEl.textContent = 0;
+    count = 0;
+}
